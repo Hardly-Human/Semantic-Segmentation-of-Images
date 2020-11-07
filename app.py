ff01f298d806719018fa1567f2b0b1d0d251ec56
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2020-11-08 00:03:17 +0530

    Added Image uploader

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ import cv2
 import numpy as np
 ctx = mx.cpu(0)
 
+@st.cache
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+
 
 def main():
 	st.title("Semantic Segmentation App for Images")
@@ -22,6 +28,17 @@ def main():
 	 `			  `[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/Hardly-Human/Semantic-Segmentation-of-Images)\
 	 `            `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://lbesson.mit-license.org/)")
 
+	image_file = st.file_uploader("Upload Image", type = ['jpg','png','jpeg'])
+
+	if image_file is None:
+		st.warning("Upload Image and Run Model  (Use Image size <300 KB for faster inference)")
+
+	if image_file is not None:
+		image1 = Image.open(image_file)
+		rgb_im = image1.convert('RGB') 
+		image2 = rgb_im.save("saved_image.jpg")
+		image_path = "saved_image.jpg"
+		st.image(image1,width = 500, height = 500)
 
 
 if __name__ == "__main__":
